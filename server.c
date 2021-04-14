7a3805be7569b5521a42df1b6bdc7384642bb3c7
Tanjoui <tanguycad@gmail.com>
2021-04-14 10:01:42 +0200

    server multiport

--- server.c
+++ server.c
@@ -16,23 +16,26 @@
 
 
 /*
-	C socket server example
+	C socket server example, handles multiple clients using threads
 */
 
 #include<stdio.h>
 #include<string.h>	//strlen
+#include<stdlib.h>	//strlen
 #include<sys/socket.h>
 #include<arpa/inet.h>	//inet_addr
 #include<unistd.h>	//write
+#include<pthread.h> //for threading , link with lpthread
+
+//the thread function
+void *connection_handler(void *);
 
 int main(int argc , char *argv[])
 {
-	//1 creation struct sockaddr
-	int socket_desc , client_sock , c , read_size;
+	int socket_desc , client_sock , c , *new_sock;
 	struct sockaddr_in server , client;
-	char client_message[2000];
 	
-	//2 appel de la fonction socket
+	//Create socket
 	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
 	if (socket_desc == -1)
 	{
@@ -45,7 +48,7 @@ int main(int argc , char *argv[])
 	server.sin_addr.s_addr = INADDR_ANY;
 	server.sin_port = htons( 8888 );
 	
-	//3 appel de la fonction bind
+	//Bind
 	if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
 	{
 		//print the error message
@@ -54,27 +57,67 @@ int main(int argc , char *argv[])
 	}
 	puts("bind done");
 	
-	//4 appel de la fonction listen
+	//Listen
 	listen(socket_desc , 3);
 	
 	//Accept and incoming connection
 	puts("Waiting for incoming connections...");
 	c = sizeof(struct sockaddr_in);
 	
-	//5.1 attente de la fonction accept
-	client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+	
+	//Accept and incoming connection
+	puts("Waiting for incoming connections...");
+	c = sizeof(struct sockaddr_in);
+	while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
+	{
+		puts("Connection accepted");
+		
+		pthread_t sniffer_thread;
+		new_sock = malloc(1);
+		*new_sock = client_sock;
+		
+		if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
+		{
+			perror("could not create thread");
+			return 1;
+		}
+		
+		//Now join the thread , so that we dont terminate before the thread
+		//pthread_join( sniffer_thread , NULL);
+		puts("Handler assigned");
+	}
+	
 	if (client_sock < 0)
 	{
 		perror("accept failed");
 		return 1;
 	}
-	puts("Connection accepted");
+	
+	return 0;
+}
+
+/*
+ * This will handle connection for each client
+ * */
+void *connection_handler(void *socket_desc)
+{
+	//Get the socket descriptor
+	int sock = *(int*)socket_desc;
+	int read_size;
+	char *message , client_message[2000];
+	
+	//Send some messages to the client
+	message = "Greetings! I am your connection handler\n";
+	write(sock , message , strlen(message));
+	
+	message = "Now type something and i shall repeat what you type \n";
+	write(sock , message , strlen(message));
 	
 	//Receive a message from client
-	while( (read_size = recv(client_sock , client_message , 2000 , 0)) > 0 )
+	while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
 	{
 		//Send the message back to client
-		write(client_sock , client_message , strlen(client_message));
+		write(sock , client_message , strlen(client_message));
 	}
 	
 	if(read_size == 0)
@@ -86,6 +129,9 @@ int main(int argc , char *argv[])
 	{
 		perror("recv failed");
 	}
+		
+	//Free the socket pointer
+	free(socket_desc);
 	
 	return 0;
 }
