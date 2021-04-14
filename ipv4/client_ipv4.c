8c2a4517864d7a08e5b27e91af4cc0a4f9cf47d9
samira <sami.younesy@gmail.com>
2021-04-14 15:29:04 +0200

    client gcc

--- ipv4/client_ipv4.c
+++ ipv4/client_ipv4.c
@@ -2,20 +2,16 @@
 #include<string.h>    //strlen
 #include<sys/socket.h>    //socket
 #include<arpa/inet.h> //inet_addr
- 
-
-
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
     int sockfd;
     char buffer[1000];
     char server_reply[2000];
     ssize_t n;
-  
-   struct sockaddr_in servaddr; 
-   
 
-  
+   struct sockaddr_in servaddr;
+
+
+
    sockfd = socket(AF_INET,SOCK_STREAM,0);
    if (sockfd == -1)
     {
@@ -26,11 +22,11 @@ int main(int argc, char **argv)
    servaddr.sin_family = AF_INET;
    servaddr.sin_port = htons(8888);
    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-   
+
    connect(sockfd, (struct sockaddr *)&servaddr,sizeof(servaddr));
    while (1)
    {
-	   
+
     printf("Entrez un message: ");
     scanf("%s",buffer);
 
